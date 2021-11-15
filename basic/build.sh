08c56dbd4d21ebbb6318c2deeee39d333c358e0d
gary.archer <gary.archer@WORKMAC.local>
2021-11-15 13:29:01 +0100

    Token handler deployment updates

--- basic/build.sh
+++ basic/build.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-########################################################################
-# A script to build code and produce Docker images, ready for deployment
-########################################################################
-
+###########################################
+# A script to build Token Handler resources
+###########################################
 
 #
-# Get and build the BFF API here
+# Get and build the main Token Handler API (aka 'OAuth Agent')
 #
 cd ..
 rm -rf bff-node-express
-git clone https://github.com/curityio/bff-node-express
+git clone https://github.com/curityio/bff-node-express token-handler-api
 if [ $? -ne 0 ]; then
-  echo "Problem encountered downloading the BFF API"
+  echo "Problem encountered downloading the token handler API"
   exit 1
 fi
 
-cd bff-node-express
+cd token-handler-api
 npm install
 if [ $? -ne 0 ]; then
-  echo "Problem encountered installing the BFF API dependencies"
+  echo "Problem encountered installing the Token Handler API dependencies"
   exit 1
 fi
 
 npm run build
 if [ $? -ne 0 ]; then
-  echo "Problem encountered building the BFF API code"
+  echo "Problem encountered building the Token Handler API code"
   exit 1
 fi
 
-docker build -f Dockerfile -t bff-api:1.0.0 .
+docker build -f Dockerfile -t token-handler-api:1.0.0 .
 if [ $? -ne 0 ]; then
-  echo "Problem encountered building the BFF API Docker file"
+  echo "Problem encountered building the Token Handler API Docker file"
   exit 1
 fi
+
+#
+# Get the 'OAuth Proxy', which is a simple reverse proxy plugin
+#
